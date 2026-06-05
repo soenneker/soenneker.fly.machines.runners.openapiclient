eeb2caec41188c2b43e1346763573b83672614f5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:12 -0500

    xml commenting

--- src/Soenneker.Fly.Machines.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Fly.Machines.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Fly.Machines.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Fly.Machines.OpenApiClient";
 }
